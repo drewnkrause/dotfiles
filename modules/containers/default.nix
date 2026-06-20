0afa5487e28b895c0aeff4d3bc761d353a3ab210
Drew Krause <drewkrause2@gmail.com>
2026-06-20 18:30:48 -0500

    Get rid of compose2nix

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -1,4 +1,5 @@
-{ ... }:
+{ config, pkgs, ... }:
+
 {
   imports = [
     # ./factorio.nix
@@ -7,4 +8,20 @@
     # ./terraria-server.nix
     ./mcxb.nix
   ];
+
+  # Runtime
+  virtualisation.podman = {
+    enable = true;
+    autoPrune.enable = true;
+    dockerCompat = true;
+  };
+
+  # Enable container name DNS for all Podman networks.
+  networking.firewall.interfaces = let
+    matchAll = if !config.networking.nftables.enable then "podman+" else "podman*";
+  in {
+    "${matchAll}".allowedUDPPorts = [ 53 ];
+  };
+
+  virtualisation.oci-containers.backend = "podman";
 }
