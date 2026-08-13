7db79881437640ccea7a6db3a95c80c37d08b0cc
Drew Krause <drewkrause2@gmail.com>
2026-08-12 19:50:08 -0500

    fix noctalia cachix

--- flake.nix
+++ flake.nix
@@ -20,8 +20,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:noctalia-dev/noctalia/cachix";
     };
     
     niri-scratchpad = {
--- modules/system/common.nix
+++ modules/system/common.nix
@@ -29,4 +29,6 @@
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDsd8WcigFSY7bXChRp1AU0oWp0Vz02ZJYudhKsXddFpAAAABHNzaDo= secondary-yubikey"
     ];
   };
+
+  nix.settings.trusted-users = [ "drew" ];
 }
